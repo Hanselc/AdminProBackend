2d876dd034ce6d99d48779468c5ae89708d83fc0
Hansel Chabur <hansel.chabur@outlook.com>
2019-04-09 00:27:08 -0500

    Partial section 16

--- routes/hospital.js
+++ routes/hospital.js
@@ -32,6 +32,33 @@ app.get('/', (req, res) => {
         });
 });
 
+// Get hospital by ID
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    Hospital.findById(id)
+        .populate('user', 'name image email')
+        .exec((err, hospital) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    message: 'Error searching hospital',
+                    errors: err
+                });
+            }
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    message: 'There is no hospital with id ' + id,
+                    errors: { message: 'There is no hospital with that ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                hospital: hospital
+            });
+        })
+})
+
 // Create new hospital
 app.post('/', auth.validateToken, (req, res) => {
     var body = req.body;
